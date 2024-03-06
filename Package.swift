971538c399f6ca56ef459a7172c6f21251991945
vladimir-n0691 <vladimir@expofp.com>
2024-03-06 12:24:48 +0300

    Release 4.3.1

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/weichsel/ZIPFoundation", exact: "0.9.17"),
-        .package(url: "https://github.com/expofp/expofp-common-ios-spm", exact: "4.2.20"),
+        .package(url: "https://github.com/expofp/expofp-common-ios-spm", exact: "4.3.1"),
     ],
     targets: [
         .target(name: "ExpoFpFplanTarget",
