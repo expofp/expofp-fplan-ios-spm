933714255ba01ac9bddf3b930536ffc71d8af559
vladimir-n0691 <vladimir@expofp.com>
2024-07-15 23:23:26 +0300

    Release 4.5.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -13,13 +13,15 @@ let package = Package(
             targets: ["ExpoFpFplanTarget"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/weichsel/ZIPFoundation", exact: "0.9.17"),
-        .package(url: "https://github.com/expofp/expofp-common-ios-spm", exact: "4.4.5"),
+        //.package(url: "https://github.com/weichsel/ZIPFoundation", exact: "0.9.19"),
+        //.package(url: "https://github.com/ZipArchive/ZipArchive.git", exact: "2.5.5"),
+        .package(url: "https://github.com/expofp/expofp-common-ios-spm", exact: "4.5.0"),
     ],
     targets: [
         .target(name: "ExpoFpFplanTarget",
                 dependencies: [
-                    .product(name: "ZIPFoundation", package: "ZIPFoundation"),
+                    //.product(name: "ZIPFoundation", package: "ZIPFoundation"),
+                    //.product(name: "ZipArchive", package: "ZipArchive"),
                     .product(name: "ExpoFpCommon", package: "expofp-common-ios-spm"),
                     .target(name: "ExpoFpFplan")
                 ],
